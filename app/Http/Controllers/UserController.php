1cba8caf0c71da63dab6a4b5035d14419ecc6aa8
QuynhPhuong <32598364+QuynhPhuong@users.noreply.github.com>
2017-12-06 20:09:23 +0700

    Controller và view phần thêm users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,42 @@ class UserController extends Controller
     	$user = User::all();
     	return view('admin.user.danhsach',['user'=>$user]);
     }
+    public function getThem(){
+        return view('admin.user.them');
+    }
 
+    public function postThem(Request $request){
+        //echo $request->Ten; //Ten nay la name cua tag input
+        $this->validate($request,
+            [
+                'name' => 'required|min:3',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:3|max:32',
+                'passwordAgain' => 'required|same:password',
+            ],[
+                'name.required' => 'Bạn chưa nhập tên',
+                'name.min' => 'Tên người dùng phải có ít nhất 3 ký tự',
+                'email.required' => 'Bạn chưa nhập email',
+                'email.email' => 'Bạn chưa nhập đúng định dạng email',
+                'email.unique' => 'Email đã tồn tại',
+                'password.required' => 'Bạn chưa nhập mật khẩu',
+                'password.min' => 'Mật khẩu phải có độ dài từ 3 đến 32 ký tự',
+                'password.max' => 'Mật khẩu phải có độ dài từ 3 đến 32 ký tự',
+                'passwordAgain.required' => 'Bạn chưa nhập lại mật khẩu',
+                'passwordAgain.same' => 'Mật khẩu nhập lại không đúng',
+            ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->level = $request->level;
+        $user->status = $request->status;
+
+        $user->save();
+
+        return redirect('admin/user/them')->with('thongbao','Thêm thành công'); // gán thêm session thongbao
+    }
     public function getDangnhapAdmin(){
     	return view('admin.login');
     }
