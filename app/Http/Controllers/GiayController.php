8638b0a2eae5a4dd22f27fdfa8e308be538e1365
SongD <yangdungde123@gmail.com>
2017-12-08 20:34:42 +0700

    Sửa chi tiết giày, xóa chi tiết giày

--- app/Http/Controllers/GiayController.php
+++ app/Http/Controllers/GiayController.php
@@ -200,4 +200,118 @@ class GiayController extends Controller
 
         return redirect('admin/giay/themchitiet/'.$id)->with('thongbao','Thêm thành công'); // gán thêm session thongbao
     }
+
+    public function getSuaChiTiet($idMauGiay){
+        $maugiay = MauGiay::find($idMauGiay);
+        return view('admin.giay.suachitiet',[
+            'maugiay'=>$maugiay
+            ]);
+    }
+
+    public function postSuaChiTiet(Request $request,$idMauGiay,$idSize){
+        $this->validate($request,
+            [
+                //'MaGiay' => 'required',
+                'Mau' => 'required',
+                'Size' => 'required',
+                'SoLuong' => 'required',
+                'GiaCu' => 'required',
+                'GiaMoi' => 'required',
+            ],[
+                //'MaGiay.required' => 'Bạn chưa nhập mã giày',
+                'Mau.required' => 'Bạn chưa nhập màu giày',
+                'Size.required' => 'Bạn chưa nhập đủ thông tin',
+                'SoLuong.required' => 'Bạn chưa nhập đủ thông tin',
+                'GiaCu.required' => 'Bạn chưa nhập đủ thông tin',
+                'GiaMoi.required' => 'Bạn chưa nhập đủ thông tin',
+            ]);
+
+        $maugiay = MauGiay::find($idMauGiay);
+        $size = Size::where('idMauGiay',$idMauGiay)->where('id',$idSize)->get();
+
+        //$maugiay->MaGiay = $request->MaGiay;
+        $maugiay->Mau = $request->Mau;
+        $maugiay->GiaCu = $request->GiaCu;
+        $maugiay->GiaMoi = $request->GiaMoi;
+        $maugiay->NoiBat = $request->NoiBat;
+        $maugiay->Status = $request->Status;
+        $maugiay->GioiTinh = $request->GioiTinh;
+
+        $size[0]->Size = $request->Size;
+        $size[0]->SoLuong = $request->SoLuong;
+
+        $size[0]->save();
+
+        $maugiay->save();
+
+        if ($request->hasFile('HinhBe')) {
+            $file = $request->file('HinhBe');
+            $duoi = $file->getClientOriginalExtension();
+            if ($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg') {
+                return redirect('admin/giay/themchitiet/'.$idMauGiay)->with('loi','Bạn chỉ được chọn file có đuôi jpg,jpeg,png');
+            }
+            $name = $file->getClientOriginalName();
+            $HinhBe = str_random(4)."_".$name;
+            while (file_exists("upload/giay/".$maugiay->MaGiay."/chinh/".$HinhBe)) {
+                $HinhBe = str_random(4)."_".$name;
+            }
+            unlink("upload/giay/".$maugiay->MaGiay."/chinh/".$maugiay->HinhBe);
+            $file->move("upload/giay/".$maugiay->MaGiay."/chinh",$HinhBe);
+            $maugiay->HinhBe = $HinhBe;
+        }
+
+        $hinhgiay = HinhGiay::where('idMauGiay',$idMauGiay)->get();
+        foreach ($hinhgiay as $element) {
+            unlink("upload/giay/".$maugiay->MaGiay."/hinhphu/".$element->Hinh);
+            $element->delete();
+        }
+
+
+        if(count($_FILES['upload']['name']) > 0){
+
+            for($i=0; $i<count($_FILES['upload']['name']); $i++) {
+                $file = $request->file('upload');
+                //$file=$_FILES['upload']['name'][$i];
+
+                $duoi = $file[$i]->getClientOriginalExtension();
+                if ($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg') {
+                    return redirect('admin/giay/themchitiet/'.$id)->with('loi','Bạn chỉ được chọn file có đuôi jpg,jpeg,png');
+                }
+                $name = $file[$i]->getClientOriginalName();
+                $Hinh = str_random(4)."_".$name;
+                while (file_exists("upload/giay/".$maugiay->MaGiay."/hinhphu/".$Hinh)) {
+                    $Hinh = str_random(4)."_".$name;
+                }
+                $filePath = "upload/giay/".$maugiay->MaGiay."/hinhphu";
+
+                $file[$i]->move($filePath,$Hinh);
+
+                $hinhgiay = new HinhGiay;
+                $hinhgiay->Hinh = $Hinh;
+                $hinhgiay->idMauGiay = $maugiay->id;
+                $hinhgiay->save();
+            }
+
+        }
+
+        return redirect('admin/giay/suachitiet/'.$idMauGiay.'/'.$idSize)->with('thongbao','Sửa thành công');
+    }
+
+    public function getXoaChiTiet($id,$idGiay){
+        $maugiay = MauGiay::find($id);
+
+        $size = Size::where('mau_giay_id',$id)->get();
+        foreach ($size as $si) {
+            $si->delete();
+        }
+
+        $hinhgiay = HinhGiay::where('mau_giay_id',$id)->get();
+        foreach ($hinhgiay as $hinh) {
+            $hinh->delete();
+        }
+
+        $maugiay->delete();
+
+        return redirect('admin/giay/chitiet/'.$idGiay)->with('thongbao','Xóa thành công');
+    }
 }
