b594b4518f71c7a6d9d8ace09637c94dd8f04524
thucdv <32638425+thucdv@users.noreply.github.com>
2017-12-08 07:14:10 +0000

    Update SlideController.php

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -57,4 +57,47 @@ class SlideController extends Controller
 
     	return redirect('admin/slide/them')->with('thongbao','Thêm thành công'); // gán thêm session thongbao
     }
+    public function getSua($id){
+    	$slide = Slide::find($id);
+    	return view('admin.slide.sua',['slide'=>$slide]);
+    }
+
+    public function postSua(Request $request,$id){
+    	
+    	$this->validate($request,
+    		[
+    			'Ten' => 'required', //dieu kien: có, trùng trong bảng thể loại value Ten
+    			'NoiDung' => 'required'
+    		],
+    		[
+    			'Ten.required' => 'Bạn chưa nhập tên thể loại',
+    			'Ten.required' => 'Bạn chưa nhập nội dung',
+    		]);
+
+    	$slide = Slide::find($id);
+    	$slide->Ten = $request->Ten;
+    	$slide->NoiDung = $request->NoiDung;
+    	$slide->link = $request->link;
+
+    	if ($request->hasFile('Hinh')) {
+            $file = $request->file('Hinh');
+            $duoi = $file->getClientOriginalExtension();
+            if ($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg') {
+                return redirect('admin/slide/them')->with('loi','Bạn chỉ được chọn file có đuôi jpg,jpeg,png');
+            }
+            $name = $file->getClientOriginalName();
+            $Hinh = str_random(4)."_".$name;
+            while (file_exists("upload/slide/".$Hinh)) {
+                $Hinh = str_random(4)."_".$name;
+            }
+            unlink("upload/slide/".$slide->Hinh);
+            $file->move("upload/slide",$Hinh);
+            $slide->Hinh = $Hinh;
+        }
+
+
+    	$slide->save();
+
+    	return redirect('admin/slide/sua/'.$id)->with('thongbao','Sửa thành công'); // gán thêm session thongbao
+    }
 }
