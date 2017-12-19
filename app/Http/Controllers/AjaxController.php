d8c26f65fb7718acd86458153202c8cbb2dbdcff
SongD <yangdungde123@gmail.com>
2017-12-19 14:01:30 +0700

    Sửa Slide controller, ajax controller

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -15,5 +15,46 @@ use Session;
 class AjaxController extends Controller
 {
     //
-    
+    public function getLoaiGiay($idBrand){
+
+    	$brandloaigiay = BrandLoaiGiay::where('brand_id',$idBrand)->get();
+    	foreach ($brandloaigiay as $blg) {
+    		echo "<option value='".$blg->loaigiay->id."'>".$blg->loaigiay->Ten."</option>";
+    	}
+    }
+
+    public function getSoLuong($idSize){
+
+    	$size = Size::where('id',$idSize)->get();
+    	foreach ($size as $si) {
+    		//echo $si->SoLuong;
+            echo "<option value='".$si->SoLuong."'>".$si->SoLuong."</option>";
+            for ($i=0; $i <= 50; $i++) {
+                if ($i == $si->SoLuong) {
+                    echo "<option selected value='".$i."'>".$i."</option>";
+                }else{
+                    echo "<option value='".$i."'>".$i."</option>";
+                }
+            }
+    	}
+    	
+    }
+
+    public function getSoLuongSize($idSize,$soLuong){
+
+        $size = Size::find($idSize);
+        $size->SoLuong = $soLuong;
+        $size->save();
+        echo "ok";
+        
+    }
+
+    public function getSoLuong2($idSize){
+
+        $size = Size::where('id',$idSize)->get();
+        foreach ($size as $si) {
+            echo $si->SoLuong;
+        }
+        
+    }
 }
